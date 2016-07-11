5ad524342332a475ff742d67b812c1470e299057
Alex F. Bokov <bokov@uthscsa.edu>
2016-07-11 09:00:25 -0500

    Automated pw gathering, moved pwinclude.xsl to pwinclude_template.xsl and put pwinclude.xsl on .gitignore, for safety

--- bash_fns.sh
+++ bash_fns.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
+source ./global_config.rc
 
 uniqx() {
     awk '{ if (!h[$0]) { print $0; h[$0]=1 } }' "$*"
 }
 
+# Creates or replaces a file called pwinclude.xsl, which contains
+# CLEARTEXT PASSWORDS FROM YOUR i2b2 SYSTEM
+# Do not check in pwinclude.xsl!!!
+getpws() {
+echo $SSHEADER > backup_pwinclude.xsl;
+for ii in `ls $JBOSS_DEPL/*-ds.xml`;
+do 
+  xsltproc getpws.xsl $ii; 
+done|grep xsl|uniqx|sed -e "s/xmlns:xsl.*name/name/" >> pwinclude.xsl
+echo "</xsl:stylesheet>" >> pwinclude.xsl  
+}
