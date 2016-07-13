730ad99f38e5880bcbd41b39439e7c5db7edfb85
Alex F. Bokov <bokov@uthscsa.edu>
2016-07-13 16:20:43 -0500

    Added a function for pulling config files

--- bash_fns.sh
+++ bash_fns.sh
@@ -23,17 +23,40 @@ echo "</xsl:stylesheet>" >> pwinclude.xsl
 
 
 # Replaces all passwords, account-names, and connections in the xml files
-# in $JBOSS_REPO_HOME with the values specified in pwinclude.xsl and 
-# setparams.xsl
+# downstream from the path set by the mandatory first argument with the 
+# values specified in pwinclude.xsl and setparams.xsl
+# Warning: as yet this ignores non-xml files, those may need manual changes
 
 # Usage (with a pwinclude.xsl and setparams.xsl, or links to them, in the 
 # current directory):
 
-# satparams
+# setparams MyFooDir
 setparams() {
-for ii in `find $JBOSS_REPO_HOME -type f -name *.xml`;
+for ii in `find confs/$1 -type f -name *.xml`;
 do
   xsltproc setparams.xsl $ii > temp.xsl; 
   mv temp.xsl $ii;
 done
+}
+
+# Copies configuration files needing frequent changes down to a subdirectory
+# of the directory 'confs'. The name of the subdirectory is specified by the
+# mandatory first argument.
+
+# Usage: 
+
+# confget MyFooDir
+
+confget() {
+  if [ -z "$1" ]; then
+    echo 'Give a short name under which to save the configuration files';
+    exit ;
+  fi;
+  savedir=$1;
+  mkdir -p confs/"$savedir";
+  eval grep -rlZ \
+    --exclude="$fexclude" \
+    --exclude-dir="$dexclude" \
+    '"<datasource\|username\|password\|user-name"' \
+    "$CONFDIRS"/* | xargs -0 -I {} cp --parents '{}' confs/"$savedir" ;
 }
